b95383ce6f7c3265fefd45b7c7aad211fdb21154
Yagnatha Konikineni <163710565+CaseinNitrate05@users.noreply.github.com>
2024-07-07 21:13:17 +0530

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame
+{
+    public static void main(String args[])
+    {
+        int guess;
+        Random random=new Random();
+        int number=random.nextInt(1,100);
+        System.out.println("random number:"+number);
+
+        while(true)
+        {
+            Scanner input=new Scanner(System.in);
+            System.out.println("Enter your guess:");
+            guess=input.nextInt();
+            if(number>guess)
+            {
+                System.out.println("Too Low");
+            }
+            if(number<guess)
+            {
+                System.out.println("Too High");
+            }
+            if(number==guess)
+            {
+                System.out.println("Correct -> +1");
+                break;
+            }
+            
+        }
+    }
+}
