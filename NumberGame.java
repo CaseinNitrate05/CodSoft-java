836781033b2e1940fbd319b16fd4b4b320c92680
Yagnatha Konikineni <163710565+CaseinNitrate05@users.noreply.github.com>
2024-07-07 21:18:07 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,34 +1,30 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGame
-{
-    public static void main(String args[])
-    {
+public class NumberGame {
+    public static void main(String args[]) {
         int guess;
-        Random random=new Random();
-        int number=random.nextInt(1,100);
-        System.out.println("random number:"+number);
+        Random random = new Random();
+        int number = random.nextInt(1, 100);
+        System.out.println("random number:" + number);
 
-        while(true)
-        {
-            Scanner input=new Scanner(System.in);
-            System.out.println("Enter your guess:");
-            guess=input.nextInt();
-            if(number>guess)
-            {
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("--------------------------------");
+
+            System.out.print("Enter your guess:");
+            guess = input.nextInt();
+            if (number > guess) {
                 System.out.println("Too Low");
             }
-            if(number<guess)
-            {
+            if (number < guess) {
                 System.out.println("Too High");
             }
-            if(number==guess)
-            {
+            if (number == guess) {
                 System.out.println("Correct -> +1");
                 break;
             }
-            
+
         }
     }
 }
